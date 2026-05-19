0b87bd42876febf7973777f92564cb8b340aa7ed
DESKTOP-F6TEN9G <efrat37764@gmail.com>
2026-05-19 23:37:43 +0300

    create a file for Employee class

---
+++ employee.js
@@ -0,0 +1,13 @@
+class Employee {
+    name;
+    age;
+    salary;
+
+    constructor(name, age, salary) {
+        this.name = name
+        this.age = age
+        this.salary = salary
+    }
+}
+
+module.exports = Employee;
